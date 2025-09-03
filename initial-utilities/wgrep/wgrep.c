4f8eb009648bcaac8a8f971f679dcaae2c3fec9d
John Campo <jjcampo@protonmail.com>
2025-09-02 22:39:18 -0700

    wgrep

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char *argv[]){
-    char buffer[512];
-    FILE *fp = NULL;
-    if(argc<1)//no args is illegal
-        exit(0);
-    for(int i = 1 ; i<argc ; i++){ //for each arg
-        fp = fopen(argv[i],"r");
-        if(fp == NULL){
-            printf("wcat: cannot open file\n");
-            exit(1);
-        }
-        while(fgets(buffer, sizeof(buffer), fp) != NULL)
-                    printf("%s", buffer);
-        fclose(fp);
+    if(argc==1){
+        printf("wgrep: searchterm [file ...]\n");
+        exit(1);
     }
+    char buffer[1024];
+    char *target = argv[1];
+    if(argc==2){
+        while(fgets(buffer, sizeof(buffer), stdin) != NULL)
+            if(strstr(buffer, target) != NULL)
+                printf("%s", buffer);
+    return 0;
+    }
+    FILE *fp = fopen(argv[2],"r");
+    if(fp == NULL){
+        printf("wgrep: cannot open file\n");
+        exit(1);
+    }
+
+    while(fgets(buffer, sizeof(buffer), fp) != NULL)
+        if(strstr(buffer, target) != NULL)
+            printf("%s", buffer);
+    fclose(fp);
     return 0;
 }
